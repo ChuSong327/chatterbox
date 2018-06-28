a8a11a847c4b6c17b86e0b0fc157f778ea50f83a
Chu Song <chuchusong23@gmail.com>
2018-06-27 23:22:37 -0700

    Added route /api/rooms/:id.

--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -3,10 +3,18 @@ const knex = require("../../db/knex");
 
 let router = express.Router();
 
+//This router is mounted at http://localhost:3000/api/rooms
 router.get("/", (req, res) => {
     knex("rooms").select().then((rooms) => {
         res.json(rooms);
-    })
-})
+    });
+});
+
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    knex("messages").select().where("room_id", id).then((messages) => {
+        res.json(messages);
+    });
+});
 
 module.exports = router;
