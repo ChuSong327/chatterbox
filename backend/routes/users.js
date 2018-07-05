0eaf62ebf98ff7b377775029ffcccd043f28df79
Chu Song <chuchusong23@gmail.com>
2018-07-05 16:47:34 -0700

    Updated user router.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -12,15 +12,17 @@ router.get('/', (req, res) => {
 //sign in
 router.post('/signin', (req, res) => {
     knex("users").select().where("username", req.body.usernameEmail).orWhere("email", req.body.usernameEmail).then((user) => {
-        console.log(user)
         res.json(user);
     });
 });
 
 //sign up
 router.post("/signup", (req, res) => {
-    knex("users").insert(req.body).then(() => {
-        res.send("Submitted!")
+    knex("users").insert(req.body)
+    .then(() => {
+        knex("users").select().where("username", req.body.username).then((user) => {
+            res.json(user);
+        })
     });
 });
 
