48db3ba7427f16234cd8d9ceb24edcf97de3dfd8
Chu Song <chuchusong23@gmail.com>
2018-07-12 13:02:43 -0700

    Update the chatroom messages route to retrieve both message's content and user information.

--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -10,11 +10,36 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/:room_id", (req, res) => {
-    const id = req.params.room_id;
-    knex("messages").select().where("room_id", id).then((messages) => {
-        res.json(messages);
-    }).then();
+// router.get("/:room_id", (req, res) => {
+//     const id = req.params.room_id;
+//     knex("messages").select().where("room_id", id).then((messages) => {
+//         res.json(messages);
+//     }).then();
+// });
+
+//get current chatroom
+router.get("/getcurrentroom/:room_id", (req, res) => {
+    const { room_id } = req.params;
+    knex("rooms").select().where("id", room_id).then((room) => {
+        res.json(room);
+    })
 });
 
+//get current room's messages and each message's user
+router.get("/getroommessages/:room_id", (req, res) => {
+    const { room_id } = req.params;
+    let response = [];
+    knex("room_messages").select().where("room_id", room_id).then((messages) => {
+        return Promise.all(
+            messages.map(message => {
+                const { user_id } = message;
+                return knex("users").select().where("id", user_id).then(user => {
+                    response.push({content: message, user: user});
+                })
+            })
+        )
+    }).then(() => {
+        res.json(response);
+    })
+})
 module.exports = router;
