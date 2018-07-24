ccc38470a0cd660fa66023c39480f58ac6721381
Chu Song <chuchusong23@gmail.com>
2018-07-23 22:08:10 -0700

    update backend to deploy

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ const app = express();
 
 //Use middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(bodyParser.json());
 
@@ -24,22 +26,21 @@ app.use("/api/messages", messagesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
-  });
+  next(createError(404));
+});
 
 // error handler
 app.use(function(err, req, res) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log("App is listening on port: " + port);
+  console.log("App is listening on port: " + port);
 });
